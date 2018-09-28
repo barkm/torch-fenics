93ecf5d29d8b32be35763232cf4f661c86fdf92e
Patrik Barkman <barkm@kth.se>
2018-09-28 16:10:01 +0200

    Change from pyadjoint to dolfin_adjoint in setup

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(name='torch-fenics',
       author='Patrik Barkman',
       author_email='barkm@kth.se',
       packages=['torch_fenics'],
-      install_requires=['pyadjoint', 'torch==0.4.1'],
-      dependency_links=['git+https://bitbucket.org/barkm/pyadjoint.git@torch-fenics#egg=pyadjoint-2017.2.0']
+      install_requires=['dolfin_adjoint', 'torch==0.4.1'],
+      dependency_links=['git+https://bitbucket.org/barkm/pyadjoint.git@torch-fenics#egg=dolfin_adjoint-2017.2.0']
       )
 
 
