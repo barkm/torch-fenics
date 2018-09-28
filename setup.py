8bbf28ee842eaaca9e12d08afe76996fdaf4c484
Patrik Barkman <barkm@kth.se>
2018-09-28 15:12:36 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='torch-fenics',
+      version='0',
+      description='PyTorch-FEniCS interface',
+      author='Patrik Barkman',
+      author_email='barkm@kth.se',
+      packages=['torch_fenics'],
+      install_requires=['dolfin_adjoint', 'torch==0.4.1'],
+      dependency_links=['git+https://bitbucket.org/barkm/pyadjoint.git@torch-fenics#egg=pyadjoint-2017.2.0']
+      )
+
+
