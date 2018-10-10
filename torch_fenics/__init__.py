4a687af9ff5e8d75dcc0400c2a2b8ace7b01ccd4
Patrik Barkman <barkm@kth.se>
2018-10-10 16:29:39 +0200

    Include numpy_fenics in __init__

--- torch_fenics/__init__.py
+++ torch_fenics/__init__.py
@@ -4,3 +4,6 @@ set_log_level(LogLevel.ERROR)
 
 from .torch_fenics import FEniCSModel
 from .torch_fenics import FEniCSModule
+
+from .numpy_fenics import fenics_to_numpy
+from .numpy_fenics import numpy_to_fenics
