26b705bb056ae2d9b91396b6414fd795b941050d
Patrik Barkman <barkm@kth.se>
2018-10-17 20:18:36 +0200

    Added requirements and removed fenics dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='torch-fenics',
       author='Patrik Barkman',
       author_email='barkm@kth.se',
       packages=['torch_fenics'],
-      install_requires=['fenics==2018.1.0', 'dolfin_adjoint==2018.1.0', 'torch==0.4.1'],
+      install_requires=['dolfin_adjoint==2018.1.0', 'torch==0.4.1'],
       dependency_links=['git+https://bitbucket.org/barkm/pyadjoint.git@torch-fenics#egg=dolfin_adjoint-2018.1.0']
       )
 
